aed31f24c073e91ddd2777d3619a05700369231a
Song Yang <songyang1218@gmail.com>
2020-09-22 19:18:57 -0400

    WIP train script

---
+++ train.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# get aliases
+source setup.sh
+
+# corpus
+corpus='/u/demorali/corpora/1g-word-lm-benchmark-r13output/training-monolingual.tokenized.shuffled/news.en-0000*-of-00100'
+
+1bword
